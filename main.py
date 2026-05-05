658acd05bbc58cea233f2968fc61d204453ab09b
amin0211 <aminyavari53@gmail.com>
2026-05-05 08:27:01 -0700

    restore main app for railway

--- main.py
+++ main.py
@@ -435,11 +435,11 @@ def main(page: ft.Page):
 
 
     
-# ft.app(
-#     target=main,
-#     view=ft.AppView.WEB_BROWSER,
-#     host="0.0.0.0",
-#     port=int(os.environ.get("PORT", 8080))
-# )
+ft.app(
+    target=main,
+    view=ft.AppView.WEB_BROWSER,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 8080))
+)
 
-ft.app(target=main)
+# ft.app(target=main)
